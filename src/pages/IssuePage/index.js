ad607f3950078e95756c271708dc8d47e3542c67
shreya shahi <shreyathakuri1372@gmail.com>
2023-01-14 18:43:15 +0545

    added the styles of issue details page

--- src/pages/IssuePage/index.js
+++ src/pages/IssuePage/index.js
@@ -20,13 +20,29 @@ const IssueListingPage = () => {
               Feat(renderToPipeableStream): Allow passing crossorigin attribute
               on bootstrapScripts
             </h1>
+            <span className="issue__status">Open</span>
           </div>
           <div className="issue__data">
             <span className="issue__number">#123</span>
             <span className="issue__author">
               Opened by <a href="#">shreya</a>
             </span>
+          </div>
+        </div>
+        <div className="issue__details">
+          <div className="title__container">
+            <TbCircleDot className="issue__svg" />
+            <h1 className="issue__title">
+              Feat(renderToPipeableStream): Allow passing crossorigin attribute
+              on bootstrapScripts
+            </h1>
             <span className="issue__status">Open</span>
+          </div>
+          <div className="issue__data">
+            <span className="issue__number">#123</span>
+            <span className="issue__author">
+              Opened by <a href="#">shreya</a>
+            </span>
           </div>
         </div>
       </div>
