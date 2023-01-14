74467f740e97318ea5aef97698187e632ed64ad4
shreya shahi <shreyathakuri1372@gmail.com>
2023-01-14 16:57:53 +0545

    added header styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import "./header.css";
 const Header = () => {
-  return <>Header</>;
+  return (
+    <>
+      <nav className="header">
+        <a className="logo">Git Hub</a>
+        <ul className="menu">
+          <li>Pull requests</li>
+          <li>Issues</li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default Header;
