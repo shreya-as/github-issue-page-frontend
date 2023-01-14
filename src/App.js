4f91f594bd56d47571f7bd88d426d2210bb1dc80
shreya shahi <shreyathakuri1372@gmail.com>
2023-01-14 16:53:30 +0545

    added header and footer in layout component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Layout from "./components/Layout";
 
 const App = () => {
-  return <>Hello App</>;
+  return (
+    <>
+      <Layout />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Layout/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../Footer";
+import Header from "../Header";
+
+const Layout = () => {
+  return (
+    <>
+      {/* header */}
+      <Header />
+      {/* footer */}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
