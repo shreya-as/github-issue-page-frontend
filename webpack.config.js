a9393b223e6b4c06c977d927f37ca0fa0b6ef504
shreya shahi <shreyathakuri1372@gmail.com>
2023-01-14 16:49:01 +0545

    initialized react app with babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"), // the bundle output path
+    filename: "bundle.js", // the name of the bundle
+  },
+  target: "web",
+  devServer: {
+    port: "3000", // port of dev server
+    open: true, //opens the browser after server is successfully started
+
+    hot: true, // enabling HMR.
+    liveReload: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/, //folder to be excluded
+        use: "babel-loader", //loader
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/, //folder to be excluded
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
