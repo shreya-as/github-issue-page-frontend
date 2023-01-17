569454a1c0a4f0ae3dd0d60d3842b38987ec2185
shreya shahi <shreyathakuri1372@gmail.com>
2023-01-17 22:18:24 +0545

    updated actions

--- src/pages/IssuePage/state/constants.js
+++ src/pages/IssuePage/state/constants.js
@@ -1,7 +1,10 @@
 export const issuePageConstants = {
+  // get issues constants
   GET_ISSUE_REQUEST: "GET_ISSUE_REQUEST",
   GET_ISSUE_SUCCESS: "GET_ISSUE_SUCCESS",
   GET_ISSUE_FAIL: "GET_ISSUE_FAIL",
+  // update current page
   UPDATE_CURRENT_PAGE: "UPDATE_CURRENT_PAGE",
+  // update query
   UPDATE_QUERY: "UPDATE_QUERY",
 };
