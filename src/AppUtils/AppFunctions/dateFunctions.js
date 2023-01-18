a17ae0c2690e46c6385fd427c10e77e43572365f
shreya shahi <shreyathakuri1372@gmail.com>
2023-01-18 11:32:33 +0545

    resolved issue

--- src/AppUtils/AppFunctions/dateFunctions.js
+++ src/AppUtils/AppFunctions/dateFunctions.js
@@ -1,25 +1,26 @@
 // calculate time
 export const getIssueCreatedTime = (date) => {
   // create a new Date object from the input date
-  const updatedDate = new Date(date);
+  const updatedDate = new Date(date).getTime();
 
   // get the current date
-  const currentDate = new Date();
+  const currentDate = new Date().getTime();
 
   // Calculate the time in milliseconds
   const timeDiff = currentDate - updatedDate;
-
+  console.log(timeDiff, "timeDiff");
   // Calculate the number of hours, days, weeks, and months that have passed
   const minutes = Math.floor(timeDiff / (1000 * 60));
   const hours = Math.floor(timeDiff / (1000 * 60 * 60));
   const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
   const weeks = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 7));
   const months = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 30));
-  const options = { month: "short", day: "numeric", year: "numeric" };
-  return timeDiff < 0
+  const options = { month: "short", day: "numeric", year: "numeric" }; //specifies the format of the date.
+  console.log(minutes, "minutes");
+  return minutes === 0
     ? `now.`
     : minutes < 60
-    ? `${minutes} minute${minutes === 1 ? "" : "s"}ago`
+    ? `${minutes} minute${minutes === 1 ? "" : "s"} ago`
     : hours < 24
     ? `${hours} hour${hours === 1 ? "" : "s"} ago`
     : days < 7
@@ -32,5 +33,5 @@ export const getIssueCreatedTime = (date) => {
       : ` ${weeks} weeks ago`
     : months === 1
     ? "last month"
-    : `on ${updatedDate.toLocaleDateString("en-US", options)}`;
+    : `on ${new Date(date).toLocaleDateString("en-US", options)}`;
 };
